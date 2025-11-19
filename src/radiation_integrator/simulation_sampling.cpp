e23c4dd04931a4bbea810ec286e1dc3767631710
Tegan Thomas <thomas.astrophysics@gmail.com>
2025-11-19 11:11:42 -0500

    used fallback

--- src/radiation_integrator/simulation_sampling.cpp
+++ src/radiation_integrator/simulation_sampling.cpp
@@ -380,7 +380,8 @@ void RadiationIntegrator::CalculateSimulationSampling(int snapshot)
           // Set fallback values if off grid
           if (b_new == n_b)
           {
-            if (fallback_nan)
+            // for the inner r region, we always assume fallback is NaN
+            if (fallback_nan || (x1 < x1_min_block))
               sample_nan[adaptive_level](m,n) = true;
             else
               sample_fallback[adaptive_level](m,n) = true;
